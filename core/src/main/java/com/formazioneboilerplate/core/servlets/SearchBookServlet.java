436d449da1a5ee3dc5b934079ccd78219acd448a
Di Crescenzo <f.di.crescenzo@accenture.com>
2024-07-05 12:16:01 +0200

    EVO - 10 - Improve the logic of the servlet by also checking for cases where the json object in the request body does not behave as requested

--- core/src/main/java/com/formazioneboilerplate/core/servlets/SearchBookServlet.java
+++ core/src/main/java/com/formazioneboilerplate/core/servlets/SearchBookServlet.java
@@ -3,8 +3,10 @@ package com.formazioneboilerplate.core.servlets;
 
 import com.day.cq.wcm.api.PageManager;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ValueMap;
 import com.day.cq.wcm.api.Page;
@@ -54,10 +56,8 @@ public class SearchBookServlet extends SlingAllMethodsServlet {
         LOG.info("## SearchBookServlet - doPost started ###");
         response.setContentType("application/json");
         JsonObject responseResult = new JsonObject();
-
         // json array che conterrà json di risposta alla ricerca
         JsonArray results = new JsonArray();
-
         // json che mandiamo in request per la ricerca
         JsonObject bodyParameter = new JsonObject();
         PrintWriter out = response.getWriter();
@@ -74,74 +74,89 @@ public class SearchBookServlet extends SlingAllMethodsServlet {
             bodyParameter = jsonParser.parse(jsonString.toString()).getAsJsonObject();
 
             //se il bodyParameter ha almeno un elemento al suo interno
-            if(bodyParameter.size()>0){
+            if(bodyParameter.size()>0 && bodyParameter.has("searchBy")){
+                JsonElement searchByJsonElement = bodyParameter.get("searchBy");
+                if(!searchByJsonElement.isJsonNull()){
+                    String searchBy = searchByJsonElement.getAsString();
+                    if(StringUtils.isNotBlank(searchBy)){
+                        if(searchBy.equals("authorName")|| searchBy.equals("category") || searchBy.equals("datePublish"))
+                        {
+                            //si separa il body della request-> tipo di ricerca - valore di ricerca
+                            String[] split = searchBy.split("#");
+                            responseResult.addProperty("searchBy", "{"+ split[0] + "}"+" + "+"{" + split[1]+"}");
 
-                String searchBy = bodyParameter.get("searchBy").getAsString();
-                //si separa il body della request-> tipo di ricerca - valore di ricerca
-                String[] split = searchBy.split("#");
-                responseResult.addProperty("searchBy", "{"+ split[0] + "}"+" + "+"{" + split[1]+"}");
+                                //qui io sto ricavando attraverso la risorsa la pagina in cui la servlet è stata chiamata
+                                Resource resource = request.getResource();
+                                Page currentPage = resource.adaptTo(Page.class);
+                                // il resourceResolver mi permette di capire se il persorso che io mando alla risolsa esiste o meno
+                                ResourceResolver resourceResolver = resource.getResourceResolver();
 
-                if(!searchBy.isEmpty()){
-                    //qui io sto ricavando attraverso la risorsa la pagina in cui la servlet è stata chiamata
-                    Resource resource = request.getResource();
-                    Page currentPage = resource.adaptTo(Page.class);
-                    // il resourceResolver mi permette di capire se il persorso che io mando alla risolsa esiste o meno
-                    ResourceResolver resourceResolver = resource.getResourceResolver();
+                                if(currentPage!= null){
+                                    // adatto la currentPage ad essere di tipo PageManager così da poter utilizzare i suoi metodi
+                                    PageManager pageManager = currentPage.getPageManager();
+                                    if(pageManager != null){
+                                        //il pageManager mi permette di richiamare le pagine a cui sono interessata attraverso il metodo getPage
+                                        Page booksParentPage = pageManager.getPage(bookParentPagePath);
+                                        Iterator<Page> booksChildren = booksParentPage.listChildren();
+                                        // se l'itiratore ha figli entra nel ciclo
+                                        while (booksChildren.hasNext()) {
+                                            // prendi il prossimo, se 0 allora prendi il primo
+                                            Page bookPage = booksChildren.next();
+                                            String bookCurrentPagePath = bookPage.getPath();
+                                            ValueMap properties = bookPage.getProperties();
 
-                    if(currentPage!= null){
-                        // adatto la currentPage ad essere di tipo PageManager così da poter utilizzare i suoi metodi
-                        PageManager pageManager = currentPage.getPageManager();
-                        if(pageManager != null){
-
-                            //il pageManager mi permette di richiamare le pagine a cui sono interessata attraverso il metodo getPage
-                            Page booksParentPage = pageManager.getPage(bookParentPagePath);
-                            Iterator<Page> booksChildren = booksParentPage.listChildren();
-                            // se l'itiratore ha figli entra nel ciclo
-                            while (booksChildren.hasNext()) {
-                                // prendi il prossimo, se 0 allora prendi il primo
-                                Page bookPage = booksChildren.next();
-                                String bookCurrentPagePath = bookPage.getPath();
-                                ValueMap properties = bookPage.getProperties();
-
-                                String searchProperty = properties.get(split[0], String.class);
-                                if(searchProperty != null && !searchProperty.isEmpty()){
-                                    JsonObject jsonPageResult = new JsonObject();
-                                    switch(split[0]){
-                                        case "authorName":
-                                        case "category":
-                                            if(searchProperty.equals(split[1])) {
-                                                jsonPageResult = checkParameters(resourceResolver, bookCurrentPagePath, searchProperty, split[1]);
-                                                if (jsonPageResult.size() > 0) {
-                                                    results.add(jsonPageResult);
+                                            String searchProperty = properties.get(split[0], String.class);
+                                            if(searchProperty != null && !searchProperty.isEmpty()){
+                                                JsonObject jsonPageResult = new JsonObject();
+                                                switch(split[0]){
+                                                    case "authorName":
+                                                    case "category":
+                                                        if(searchProperty.equals(split[1])) {
+                                                            jsonPageResult = checkParameters(resourceResolver, bookCurrentPagePath, searchProperty, split[1]);
+                                                            if (jsonPageResult.size() > 0) {
+                                                                results.add(jsonPageResult);
+                                                            }
+                                                        }
+                                                        break;
+                                                    case "datePublish":
+                                                        LocalDate dateRequest = LocalDate.parse(split[1]);
+                                                        LocalDate dateBookPublish = LocalDate.parse(searchProperty.split("T")[0]);
+                                                        if(dateBookPublish.isAfter(dateRequest)){
+                                                            jsonPageResult = checkParameters(resourceResolver, bookCurrentPagePath, dateBookPublish.toString(), split[1]);
+                                                            if(jsonPageResult.size()>0){
+                                                                results.add(jsonPageResult);
+                                                            }
+                                                        }
+                                                        LOG.info("LocalDate.parse(searchProperty.split(\"T\")[0]).isBefore(LocalDate.parse(split[1]))");
+                                                        break;
+                                                    default:
+                                                        break;
                                                 }
                                             }
-                                            break;
-                                        case "datePublish":
-                                            LocalDate dateRequest = LocalDate.parse(split[1]);
-                                            LocalDate dateBookPublish = LocalDate.parse(searchProperty.split("T")[0]);
-                                            if(dateBookPublish.isAfter(dateRequest)){
-                                                jsonPageResult = checkParameters(resourceResolver, bookCurrentPagePath, dateBookPublish.toString(), split[1]);
-                                                if(jsonPageResult.size()>0){
-                                                    results.add(jsonPageResult);
-                                                }
-                                            }
-                                            LOG.info("LocalDate.parse(searchProperty.split(\"T\")[0]).isBefore(LocalDate.parse(split[1]))");
-                                            break;
-                                        default:
-                                            break;
+                                        }
+                                        responseResult.add("results", results);
                                     }
                                 }
-                            }
-                            responseResult.add("results", results);
+                        } else {
+                            responseResult.addProperty("{error", "the value of searchBy parameter in request body is not valid}");
+                            LOG.info("## SearchBookServlet -  the value of searchBy parameter in request body is not valid ###");
                         }
+                    } else {
+                        responseResult.addProperty("{error", "the value of searchBy parameter in request body is empty}");
+                        LOG.info("## SearchBookServlet -  the value of searchBy parameter in request body is empty ###");
                     }
+                } else {
+                    responseResult.addProperty("{error", "the value of searchBy parameter in request body is null}");
+                    LOG.info("## SearchBookServlet -  the value of searchBy parameter in request body is null ###");
                 }
+            } else {
+                responseResult.addProperty("{error", "missing searchBy parameter in request body}");
+                LOG.info("## SearchBookServlet -  missing searchBy parameter in request body ###");
             }
             out.write(responseResult.toString());
         } catch (IOException e) {
             LOG.error("## SearchBookServlet - doPost failed IOException launched ###", e);
-        }
-        finally{
+        } finally{
             out.close();
         }
 
