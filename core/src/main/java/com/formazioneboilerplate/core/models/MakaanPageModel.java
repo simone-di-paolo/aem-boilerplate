6ca9227090e9ece1f2b52ead57225caddcc5b066
Di Crescenzo <f.di.crescenzo@accenture.com>
2024-08-01 12:45:14 +0200

    Feature 02 - Create Header Component

---
+++ core/src/main/java/com/formazioneboilerplate/core/models/MakaanPageModel.java
@@ -0,0 +1,39 @@
+package com.formazioneboilerplate.core.models;
+
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+
+@Model(adaptables = {Resource.class, SlingHttpServletRequest.class}, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class MakaanPageModel {
+
+    private static final String CONGIF_PAGE_PTH = "/content/formazioneboilerplate/language-masters/en/makaan-home";
+
+    @Inject
+    private Page currentPage;
+
+    private String logoImage;
+
+    @PostConstruct
+    protected void init() {
+        //Resource currentePageResource = currentPage.adaptTo(Resource.class);
+        PageManager pageManager = currentPage.getPageManager();
+        Page spaPage = pageManager.getPage(CONGIF_PAGE_PTH);
+
+        ValueMap properties = spaPage.getProperties();
+        logoImage = properties.get("logoImage", String.class);
+
+    }
+
+    public String getLogoImage() {
+        return logoImage;
+    }
+}
