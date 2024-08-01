7ab9a215f6cfb51586c77b433925bec5baadcd7a
Di Crescenzo <f.di.crescenzo@accenture.com>
2024-08-01 12:49:05 +0200

    Feature 03 - Create Footer Component

---
+++ core/src/main/java/com/formazioneboilerplate/core/models/MakaanFooter.java
@@ -0,0 +1,24 @@
+package com.formazioneboilerplate.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@Model(adaptables = { Resource.class }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public interface MakaanFooter {
+
+    @Inject
+    List<MenuItem> getMenuItems();
+
+    @Model(adaptables = { Resource.class }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+    interface MenuItem {
+        @Inject
+        String getFooterLinkLabel();
+
+        @Inject
+        String getFooterLink();
+    }
+}
