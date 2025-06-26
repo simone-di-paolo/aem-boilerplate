28719c4a2d1ec81a5df4849edb94539a493a9012
Giosi <chiara.giosi@accenture.com>
2025-06-26 16:24:39 +0200

    logic js for the update of the herobanner component

---
+++ ui.apps/src/main/content/jcr_root/apps/formazioneboilerplate/components/herobanner/clientlib-custom/js/herobanner.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const btn = document.getElementById('button');
+    const mexAlert = document.getElementById('mex');
+    const overlay = document.getElementById('overlay-alert');
+    const closeBtn = document.getElementById('closebtn');
+    const numerorandom = document.getElementById('Nrandom');
+
+
+    function numRandom() {
+        let min = 0;
+        let max = 100;
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    btn.addEventListener("click", mostraNumeroRandom);
+    function mostraNumeroRandom() {
+        const numero = numRandom();
+        if (numero % 2 === 0) {
+            numerorandom.textContent = "Il tuo numero fortunato è " + numero + " ed è pari";
+        } else {
+            numerorandom.textContent = "Il tuo numero fortunato è " + numero + " ed è dispari";
+        }
+        mexAlert.style.display = 'block';
+        overlay.style.display = 'block';
+    }
+
+    closeBtn.addEventListener("click", closeAlert);
+    function closeAlert() {
+        mexAlert.style.display = 'none';
+        overlay.style.display = 'none';
+    }
+
+});
