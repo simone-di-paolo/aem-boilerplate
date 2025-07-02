dcf5aaf927a832945cc1fb47f39723347564bf76
Giosi <chiara.giosi@accenture.com>
2025-07-02 16:23:56 +0200

    modified form and form-recupera-dati components

--- core/src/main/java/com/formazioneboilerplate/core/models/FormModel.java
+++ core/src/main/java/com/formazioneboilerplate/core/models/FormModel.java
@@ -1,43 +1,29 @@
 package com.formazioneboilerplate.core.models;
 
-import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
-
-import javax.annotation.PostConstruct;
-import javax.inject.Inject;
-
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
-import org.apache.sling.models.annotations.injectorspecific.OSGiService;
-import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-import com.day.cq.wcm.api.Page;
-import com.day.cq.wcm.api.PageManager;
-
 import java.util.Date;
-import java.text.SimpleDateFormat;
-import java.util.Optional;
 
-@Model(adaptables = Resource.class)
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class FormModel {
 
     @ValueMapValue
-    Date datadinascita;
+    private Date datadinascita;
     @ValueMapValue
-    String nazionalita;
+    private String nazionalita;
     @ValueMapValue
-    String indirizzo;
+    private String indirizzo;
     @ValueMapValue
-    boolean checkbox;
+    private String checkbox;
     @ValueMapValue
-    boolean newsletter;
+    private String newsletter;
     @ValueMapValue
-    String labelbottone;
+    private String labelbottone;
     @ValueMapValue
-    String linkbottone;
+    private String linkbottone;
 
     public Date getDatadinascita() {
         return datadinascita;
@@ -51,11 +37,11 @@ public class FormModel {
         return indirizzo;
     }
 
-    public boolean isCheckbox() {
+    public String isCheckbox() {
         return checkbox;
     }
 
-    public boolean isNewsletter() {
+    public String isNewsletter() {
         return newsletter;
     }
 
--- ui.apps/src/main/content/jcr_root/apps/formazioneboilerplate/components/form/clientlib-custom/js/form.js
+++ ui.apps/src/main/content/jcr_root/apps/formazioneboilerplate/components/form/clientlib-custom/js/form.js
@@ -1,15 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById('btn');
 
-    const btn = document.getElementById('bottone');
-    const datanascita = document.getElementById('datanascita');
-    const indirizzo = document.getElementById('indirizzo');
-    const checkbox = document.getElementById('checkbox');
-    const newsletter = document.getElementById('newsletter');
+    btn.addEventListener("click", (event) => {
+        event.preventDefault();
 
-    btn.addEventListener("click", apripagina);
-    function apripagina() {
-    window.open("/content/formazioneboilerplate/us/en/task2/form-recupera-dati.html", "_blank");
 
-    }
+        const datadinascita = document.getElementById('datadinascita').value;
+        const nazionalita = document.getElementById('nazionalita').value;
+        const indirizzo = document.getElementById('indirizzo').value;
+        const checkbox = document.getElementById('checkbox').value;
+        const newsletter = document.getElementById('newsletter').value;
 
+
+        sessionStorage.setItem('datadinascita', datadinascita);
+        sessionStorage.setItem('nazionalita', nazionalita);
+        sessionStorage.setItem('indirizzo', indirizzo);
+        sessionStorage.setItem('checkbox', checkbox);
+        sessionStorage.setItem('newsletter', newsletter);
+
+
+        window.open("/content/formazioneboilerplate/us/en/form-recupera-dati.html", "_blank");
+    });
 });
