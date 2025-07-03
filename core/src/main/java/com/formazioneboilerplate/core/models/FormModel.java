87e356702a0f6bf5c65056642ec3231889aca2df
Giosi <chiara.giosi@accenture.com>
2025-07-03 14:50:58 +0200

    created feedback component

--- core/src/main/java/com/formazioneboilerplate/core/models/FormModel.java
+++ core/src/main/java/com/formazioneboilerplate/core/models/FormModel.java
@@ -1,11 +1,12 @@
 package com.formazioneboilerplate.core.models;
 
+
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
-
 import java.util.Date;
+
 
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class FormModel {
@@ -26,30 +27,37 @@ public class FormModel {
     private String linkbottone;
 
     public Date getDatadinascita() {
+
         return datadinascita;
     }
 
     public String getNazionalita() {
+
         return nazionalita;
     }
 
     public String getIndirizzo() {
+
         return indirizzo;
     }
 
     public String isCheckbox() {
+
         return checkbox;
     }
 
     public String isNewsletter() {
+
         return newsletter;
     }
 
     public String getLabelbottone() {
+
         return labelbottone;
     }
 
     public String getLinkbottone() {
+
         return linkbottone;
     }
 }
