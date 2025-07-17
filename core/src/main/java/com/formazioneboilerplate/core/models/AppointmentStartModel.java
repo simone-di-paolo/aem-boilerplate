616212ecae95e73f22c6e7944ac33ea36e5721f3
Merico <benedetta.merico@accenture.com>
2025-07-17 14:21:42 +0200

    I created a form component nested in a start component with a js logic to retrieve data of the form in a new page on the button submit

---
+++ core/src/main/java/com/formazioneboilerplate/core/models/AppointmentStartModel.java
@@ -0,0 +1,23 @@
+package com.formazioneboilerplate.core.models;
+
+import lombok.Getter;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+
+@Getter
+@Model(adaptables = Resource.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+
+public class AppointmentStartModel {
+
+        @ValueMapValue
+        private String title;
+
+        @ValueMapValue
+        private String image;
+
+}
+
